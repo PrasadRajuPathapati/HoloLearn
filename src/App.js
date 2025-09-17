728ea69353f4c9124a3115da8963a3e718638ca2
PrasadRajuPathapati <vtu20327.soc.cse@gmail.com>
2025-09-17 21:14:19 +0530

    page2 video update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import FeaturesHome from "./pages/FeaturesHome";
+import HomePage from "./pages/HomePage";      // your landing page
+import FeaturesHome from "./pages/FeaturesHome";  // the new home with features
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/features" element={<FeaturesHome />} />
+        <Route path="/" element={<HomePage />} />      {/* Landing page */}
+        <Route path="/features" element={<FeaturesHome />} />  {/* Full features home */}
       </Routes>
     </Router>
   );
